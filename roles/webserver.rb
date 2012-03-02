d9cba579e182f70f1fed48ab4b9be30a4a137a4f
Sudarshan <passinthrunow@gmail.com>
2012-03-02 16:36:52 +0000

    also listen at 8080

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -3,4 +3,4 @@ description "This is the web server role"
 run_list(
   "recipe[apache2]"
 )
-default_attributes "apache2" => { "listen_ports" => [ "80", "8080", "443" ] }
+override_attributes "apache" => { "listen_ports" => [ "80", "8080", "443" ] }
