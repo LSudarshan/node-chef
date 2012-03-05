c972da556d8661f7a083ca3c13f560d8eea8ff2a
Sudarshan <passinthrunow@gmail.com>
2012-03-05 14:35:43 +0000

    adding reverse proxy configuration for this setup using a new cookbook called webserver

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -1,6 +1,6 @@
 name "webserver"
 description "This is the web server role"
 run_list(
-  "recipe[apache2]", "recipe[web-server::apache-reverse-proxy]"
+  "recipe[apache2]", "recipe[apache2::mod_proxy]", "recipe[apache2::mod_proxy_http]", "recipe[web-server::apache-reverse-proxy]"
 )
 override_attributes "apache" => { "listen_ports" => [ "80", "8080", "443" ] }
